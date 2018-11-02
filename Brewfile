d22b28ada34c361f6453f267d6f6e94aad560bf0
Magnus Mortensen <magnusm@microsoft.com>
2018-11-02 09:40:07 +0100

    Config fixes
    Config fixes

---
+++ Brewfile
@@ -0,0 +1,4 @@
+brew "git"
+brew "node@8", link: true
+brew "watchman"
+brew "yarn"
--- tools/clean.js
+++ tools/clean.js
@@ -1,7 +1,3 @@
-// This is implemented in .js instead of .ts so we don't need to depend on ts-node etc.
-// Running yarn run clean in a previously cleaned repository will be very common.
-// We don't want a requirement to do `yarn` before you can do `yarn run clean`.
-
 const fs = require("fs");
 const pathLib = require("path");
 const process = require("process");
@@ -19,15 +15,12 @@ function deleteFolderRecursive(path) {
     }
     listFolder(path).forEach(file => {
         if (fs.lstatSync(file).isDirectory()) {
-            // Folder - recurse into it.
             deleteFolderRecursive(file);
         } else {
-            // File - unlink it.
             fs.unlinkSync(file);
         }
     });
 
-    // Folder is now empty, remove it.
     fs.rmdirSync(path);
 }
 
